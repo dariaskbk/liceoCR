5c182ba90dedc2ea99f33738dea3169850d59cc1
dariaskbk <30334082+dariaskbk@users.noreply.github.com>
2017-07-27 17:54:24 -0600

    David
    se me fue esto de más en la carpeta css

--- css/circulares.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-require "views/circulares.view.php";
-
-?>
--- css/index.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-  ini_set('date.timezone', 'America/Costa_Rica');
-
-  $fecha = date('Y-m-d');
-
-  require "views/index.view.php";
-
-?>
